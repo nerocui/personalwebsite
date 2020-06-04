b6b3d7c6ebc100d1ac0d53e8ed6d026f62e51d90
Nero Cui <nerocui@outlook.com>
2020-06-04 21:29:14 +0000

    Updates

---
+++ precache-manifest.45f57f25bf0cbd8b02fbd9963ae01c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5feb5f8c68b115e7af8dcb15b1625ffd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d547a26ddbdd7a86873c",
+    "url": "/static/css/main.f0064055.chunk.css"
+  },
+  {
+    "revision": "7d39308d8f06f85233de",
+    "url": "/static/js/2.09851ab6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.09851ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d547a26ddbdd7a86873c",
+    "url": "/static/js/main.d5a33077.chunk.js"
+  },
+  {
+    "revision": "400c1075147db57919c4",
+    "url": "/static/js/runtime-main.6f85cc37.js"
+  }
+]);
